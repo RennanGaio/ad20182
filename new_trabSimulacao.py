c6b3006aaf4165d267fe80eb3387574bc137189a
Rennan Gaio <rennan.gaio12@gmail.com>
2018-12-11 11:13:57 -0200

    codigo do simulador completo, falta ajustar testes de IC

--- new_trabSimulacao.py
+++ new_trabSimulacao.py
@@ -21,6 +21,7 @@ import time
 from scipy import stats
 import math
 import random
+from scipy.stats import chi2
 import sys
 
 #TODO
@@ -98,28 +99,34 @@ class Simulador:
 
         #self.total_clientes = 0
         self.servidor_ocupado = False
-        #-1 pois sera a fase transiente
+        #-1 pois comeca na fase transiente
         self.rodada_atual = -1
-
-        #variável que irá guardar a área a cada chegada à fila e a cada entrada em serviço
-        #nos eventos de interesse
-        self.area = 0
 
         #listas do Simulador
         #lista de eventos que vai comandar a ordem em que acontecem as chegadas e saidas
         self.lista_de_eventos = []
+
         #self.fila = deque()
 
         #fila da MM1, que terao clientes esperando para serem atendidos
         self.fila_de_fregueses = []
 
-        #self.debug = False
-
         #lista para salvar dados dos clientes para a geracao de graficos
         self.todos_fregueses_atendidos = []
 
+        #listas que irao guardar metricas
         self.W_barra_por_rodada = []
+
+        #variaveis necessarias para o calculo de nq
         self.Nq_barra_por_rodada = []
+        self.fregueses_na_fila_evento_anterior = 0
+        self.tempo_evento_anterior = 0.0
+        self.tempo_inicio_rodada = 0.0
+        #variável que irá guardar a área a cada chegada à fila e a cada entrada em serviço
+        #nos eventos de interesse
+        self.area_fregueses_tempo = 0
+
+        #lista de fregueses completos por rodada, que tambem sera usado para gerar as medias por rodada
         self.fregueses_atendidos_rodada = []
 
     def simulaTempoExponencial(self, taxa):
@@ -128,6 +135,15 @@ class Simulador:
         # economizamos 1 operacao de subtracao por numero gerado
         tempo = -math.log(r)/taxa
         return tempo
+
+    #funcao auxiliar para o calculo de pessoas na fila
+    def somaArea(self):
+        self.area_fregueses_tempo += (self.tempo - self.tempo_evento_anterior ) * self.fregueses_na_fila_evento_anterior
+
+    #funcao para calcular a quantidade media de pessoas na fila da mm1
+    def calculaNq(self):
+        tempo_da_rodada = self.tempo - self.tempo_inicio_rodada
+        self.Nq_barra_por_rodada.append(self.area_fregueses_tempo/tempo_da_rodada)
 
     def inserirEventoEmOrdem(self, evento):
         self.lista_de_eventos.append(evento)
@@ -160,18 +176,26 @@ class Simulador:
         if center/10 < (upper - lower):
             self.is_transiente=False
 
+    def adicionaWBarraDaRodada(self):
+        n = len(self.fregueses_atendidos_rodada)
+        tempos_de_fila = [fregues.tempoEmEspera() for fregues in self.fregueses_atendidos_rodada]
+        self.W_barra_por_rodada.append(np.sum(tempos_de_fila)/n)
+
 
 
     def iniciaProcesso(self):
         #cria o primeiro evento de chegada para dar inicio ao simulador
         self.inserirEventoEmOrdem(self.geraEventoChegada(Fregues(self.rodada_atual)))
 
-        while self.rodada_atual <= self.n_rodadas:
+        while self.rodada_atual < self.n_rodadas:
             #print self.lista_de_eventos
             #funcao pop(0) retira o primeiro elemento da lista, que e o proximo evento que ira acontecer em ordem cronologica
             evento_atual = self.lista_de_eventos.pop(0)
 
             #print evento_atual.tipo_de_evento
+
+            #verifica quantidade de pessoas que exitiam na fila antes de tratar o proximo evento
+            self.fregueses_na_fila_evento_anterior = len(self.fila_de_fregueses)
 
             #testa para ver o tipo de evento que esta sendo tratado
             if evento_atual.tipo_de_evento == "evento_chegada":
@@ -184,7 +208,7 @@ class Simulador:
                 #adiciona o fregues a fila da MM1
                 self.fila_de_fregueses.append(evento_atual.fregues)
 
-                #cria uma nova chegada de fregues
+                #cria uma nova chegada de fregues na lista de eventos
                 self.inserirEventoEmOrdem(self.geraEventoChegada(Fregues(self.rodada_atual)))
 
             #se o evento nao e de entrada, entao ele e de saida
@@ -195,15 +219,25 @@ class Simulador:
                 #a classe fregues recebe seu tempo de saida de acordo com o tempo do Evento que ocasionou a saida desse fregues
                 evento_atual.fregues.tempo_termino_servico = self.tempo
 
-                # servidor nao esta mais ocupado
+                # servidor deixa de estar ocupado
                 self.servidor_ocupado = False
 
-                # adicionando a lista de clientes atendidos
+                # adicionando a lista de todos os clientes atendidos para metricas globais
                 self.todos_fregueses_atendidos.append(evento_atual.fregues)
 
-                # adicionando a lista de clientes atendidos nesta rodada
+                # adicionando a lista de clientes atendidos nesta rodada para metricas locais
                 self.fregueses_atendidos_rodada.append(evento_atual.fregues)
 
+            #depois que um dos 2 eventos ocorreu, eu irei calcular a area de fregueses que esses eventos provocaram
+
+            #faz calculo da area de pessoas por tempo para ser utilizado para calcular Nq_barra_por_rodada
+            self.somaArea()
+
+            #atualiza o valor do tempo do evento que acabou de acontecer, o proximo evento anterior
+            self.tempo_evento_anterior = self.tempo
+
+            #Se apois os eventos ocorrerem, existir alguem na fila e o servidor estiver acabado de ser liberado
+            #Entao o programa vai servir o proximo fregues que esta em espera, com relacao a politica de atendimento
             if len(self.fila_de_fregueses) != 0 and not self.servidor_ocupado:
                 #Se FCFS, eu irei tirar da fila de fregueses o fregues que entrou a mais tempo, o fregues da esquerda
                 if self.tipo_fila == "FCFS":
@@ -214,27 +248,100 @@ class Simulador:
 
                 #atualiza o tempo em que o fregues entrou em servico
                 fregues.tempo_comeco_servico = self.tempo
+
                 #gera o evento de saida que essa entrada em servico ira ocasionar
                 self.inserirEventoEmOrdem(self.geraEventoSaida(fregues))
 
                 #servidor passa a estar ocupado
                 self.servidor_ocupado = True
 
-
             if len(self.fregueses_atendidos_rodada) >= self.min_k:
                 if self.is_transiente:
+                    #print "teste transiente"
                     self.testeFaseTransiente()
-                    print "fase transiente"
-                    print self.is_transiente
+                    #print "fase transiente"
+                    #print self.is_transiente
+                    #caso chegue ao fim da fase transiente, entao comecamos a rodada 0
+                    if not self.is_transiente:
+                        self.rodada_atual +=1
+                        self.fregueses_atendidos_rodada = []
+                        self.tempo_inicio_rodada = self.tempo
+                        self.area_fregueses_tempo = 0
                 else:
                     #gera metricas e estatisticas
+                    self.calculaNq()
+                    #print "fregueses atendidos na rodada: "+str(len(self.fregueses_atendidos_rodada))
+                    self.adicionaWBarraDaRodada()
 
                     # limpando os clientes atendidos nesta rodada
                     self.fregueses_atendidos_rodada = []
+                    self.area_fregueses_tempo = 0
+                    self.tempo_inicio_rodada = self.tempo
 
                     # proxima rodada
-                    #print 'Termino da rodada #%d' % self.rodada
+                    #print "rodada: " + str(self.rodada_atual)
                     self.rodada_atual += 1
+
+
+def ICDaMedia(mean_list):
+    #percentil da T-student para mais de 120 amostras
+    percentil = 1.645
+
+    #qtd de amostras
+    n = len(mean_list)
+
+    #média amostral
+    mean = np.sum(mean_list)/n
+
+    #variancia amostral = SUM((Media - Media Amostral)^2) = S^2
+    s = math.sqrt(np.sum( [(float(element) - float(mean))**2 for element in mean_list] ) / (n-1.0))
+
+    #calculo do Intervalo de Confiança pela T-student
+    lower = mean - (percentil*(s/math.sqrt(n)))
+    upper = mean + (percentil*(s/math.sqrt(n)))
+
+    center = lower + (upper - lower)/2
+
+    if center/10 > (upper - lower):
+        print "teste IC da media não obteve precisao de 5%, intervalo maior do que 10% do valor central"
+
+    #retorna o limite inferior, limite superior, o valor central e a precisão, nessa ordem.
+    return (lower, upper, center)
+
+
+def ICDaVariacia(mean_list):
+    #esse método utilizará a formula do qui-quadrado para medir a variancia
+
+    #qtd de amostras
+    n = len(mean_list)
+
+    #média amostral
+    mean = np.sum(mean_list)/n
+
+    #dados obtidos na tabela da qui-quadrada para alpha=0.5, alpha/2 = 0.25
+    #Qalpha2 = 74.222
+    #Q1menosalpha2 = 129.561
+
+    #como na tabela de qui-quadrado só temos ate 100 graus de liberdade, tivemos que usar uma funcao
+    #auxiliar para calcular o valor dela para n = 3200
+
+    Qalpha2 = chi2.isf(q=0.025, df=n-1)
+    Q1menosalpha2 = chi2.isf(q=0.975, df=n-1)
+
+    #variancia amostral = SUM((Media - Media Amostral)^2) = S^2
+    s_quadrado=np.sum( [(float(element) - float(mean))**2 for element in mean_list] ) / (n-1.0)
+
+    #calculo do Intervalo de Confiança pela qui-quadrado
+    lower = (n-1)*s_quadrado/Q1menosalpha2
+    upper = (n-1)*s_quadrado/Qalpha2
+
+    center = lower + (upper - lower)/2
+
+    if center/10 > (upper - lower):
+        print "teste IC da variancia não obteve precisao de 5%, intervalo maior do que 10% do valor central"
+
+    #retorna o limite inferior, limite superior, o valor central e a precisão, nessa ordem.
+    return (lower, upper, center)
 
 
 
@@ -243,8 +350,9 @@ class Simulador:
 if __name__ == '__main__':
     vetor_lamb = [0.2, 0.4, 0.6, 0.8, 0.9]
     mi = 1
-    kmins = [100, 150, 200]
+    kmins = [100, 300, 500, 700, 1000]
     n_rodadas = 3200
+    #n_rodadas = 10
     n_tipos_fila = ["FCFS", "LCFS"]
     IC = 0.95
     precisao = 0.05
@@ -258,7 +366,18 @@ if __name__ == '__main__':
                 simulador.iniciaProcesso()
 
                 #testando qualquer variavel para ver a corretude do simulador
-                freg = simulador.todos_fregueses_atendidos
-                print freg
+                nqbarra = simulador.Nq_barra_por_rodada
+                wbarra = simulador.W_barra_por_rodada
 
-                #break
+                lowerM, upperM, centerM = ICDaMedia(wbarra)
+                lowerV, upperV, centerV = ICDaVariacia(nqbarra)
+
+                print "fim da rodada com lamb = " + str(lamb) + " k = " + str(k) + " tipo de fila = " + tipo_fila
+
+
+
+
+
+        #         break
+        #     break
+        # break
